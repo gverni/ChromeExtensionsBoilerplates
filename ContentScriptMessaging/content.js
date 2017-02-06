98c517eff6b744be9ac71223ecb9295b5b53bc87
Verni <gverni@qti.qualcomm.com>
2017-02-06 19:30:45 +0000

    First commit

---
+++ ContentScriptMessaging/content.js
@@ -0,0 +1,11 @@
+
+// This function inits the listener for messages coming from the popup
+chrome.runtime.onConnect.addListener(function (port) {
+  port.onMessage.addListener(function (msg) {
+
+  	console.log("Content.js: Message received")
+    // Implement handling of messages, eg an echo message: 
+	port.postMessage({echo: msg})
+   
+  })
+})
