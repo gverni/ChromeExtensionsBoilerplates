439753a6509af3b2e16b5374c7205d3c847e439b
gverni <g.verni@gmail.com>
2017-03-12 22:43:38 +0000

    First working version of Background-Content-View boilerplate

---
+++ BackgroundContentView/background.js
@@ -0,0 +1,48 @@
+// Variable that hold the communciation channel with content script
+var portView
+var portContent 
+
+function connectToContent(tabId) {
+ 
+    portContent = chrome.tabs.connect(tabId)
+    portContent.onMessage.addListener(function (msg) { handleMessageFromContent(msg) }) 
+
+}
+
+function handleMessageFromContent(msg) {
+  if (msg.dst == "view") {
+    sendMessageToView(msg)
+  }
+}
+
+
+function sendMessageToContent(msg) {
+	portContent.postMessage(msg)
+}
+
+
+function handleMessageFromView(msg) {
+  if ((msg.dst == "content") && (msg.cmd == "get-data")) {
+    // Send get-data to content
+    console.log("msg received from view: Received get-data")
+    sendMessageToContent({src: "view", cmd: "get-data"})
+  }
+}
+
+function sendMessageToView(msg) {
+  console.log("Send messsge to view")
+  portView.postMessage(msg)
+}
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+
+  connectToContent(tab.id)
+
+  chrome.tabs.create({ url: "./view.html"}, function (tab) {
+    chrome.runtime.onConnect.addListener(function(port) {
+      portView = port
+      portView.onMessage.addListener(function(msg) { handleMessageFromView(msg) })
+    })
+  })
+
+})
---
+++ BackgroundContentView/content.js
@@ -0,0 +1,12 @@
+
+// This function inits the listener for messages coming from the background
+chrome.runtime.onConnect.addListener(function (port) {
+  port.onMessage.addListener(function (msg) {
+  	if (msg.src == "view") {
+  	  if (msg.cmd == "get-data") {
+  	    console.log("Content: get-data received")
+	    port.postMessage({dst: "view", cmd: "rsp-data", data: "Hello"})
+  	  } 
+  	}
+  })
+})
