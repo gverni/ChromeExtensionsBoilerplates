bb0d5202384f0b11f04ec2c6201e157c3a0abb2c
Giuseppe Verni <gverni@qti.qualcomm.com>
2017-03-13 10:49:15 +0000

    First commit for popup bolierplate/sample

---
+++ Popup/popup.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+  
+   document.getElementById('message').textContent = "Hello World";
+   // window.open("http://google.com")
+   console.log("Hello World - Log message")
+   
+});
