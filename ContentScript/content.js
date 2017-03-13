83367f692ba3271a46276658fd213e811651acf6
Giuseppe Verni <gverni@qti.qualcomm.com>
2017-03-13 10:49:47 +0000

    First commit for ContentScript boilerplate / sample

---
+++ ContentScript/content.js
@@ -0,0 +1,15 @@
+function beauitfyClass(className) {
+
+	var elementsToBeautify = document.getElementsByClassName(className)
+	while (elementsToBeautify.length > 0) {
+		elementsToBeautify[0].className = "beautify" + className
+	}
+
+}
+
+beauitfyClass('btn')
+
+var images = document.getElementsByTagName('img');
+for (var i = 0, l = images.length; i < l; i++) {
+  images[i].src = 'http://placekitten.com/' + images[i].width + '/' + images[i].height;
+}
